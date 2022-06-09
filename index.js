4937c3859c52410ed3894cdc2b22d9a6235650e7
Vladislav Tselishchev <vlad.tselischev@omega-r.com>
2022-06-09 17:17:55 +0300

    feat: add fileservice logic

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
-import router from './router.js';
+import router from "./router.js";
+import fileUpload from "express-fileupload";
 
 const PORT = 3000;
 const DB_URL = "mongodb+srv://user:user@cluster0.458zkjw.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
+
 app.use(express.json());
-app.use('/api', router)
+app.use(fileUpload({}));
+app.use(express.static("static"));
+app.use("/api", router);
 
 async function startApp() {
     try {
--- router.js
+++ router.js
@@ -1,13 +1,12 @@
-import Router from 'express'
-import PostController from './PostController.js'
+import Router from "express";
+import PostController from "./PostController.js";
 
-const router = new Router()
+const router = new Router();
 
-router.post('/posts', PostController.create)
-router.get('/posts', PostController.getAll)
-router.get('/posts/:id', PostController.getOne)
-router.put('/posts/:id', PostController.update)
-router.delete('/posts/:id', PostController.delete)
+router.post("/posts", PostController.create);
+router.get("/posts", PostController.getAll);
+router.get("/posts/:id", PostController.getOne);
+router.put("/posts/:id", PostController.update);
+router.delete("/posts/:id", PostController.delete);
 
-export default router
-
+export default router;
